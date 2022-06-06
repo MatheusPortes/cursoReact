43af95bcea1a8d48a54d6d505dcef1c33a2286a8
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-06-05 22:00:53 -0300

    Ajustes no submenu / Submenu settings.

--- src/Components/SubMenu/SubMenu.js
+++ src/Components/SubMenu/SubMenu.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Peges } from '../../Api/api'
+import React, { useState, useEffect } from 'react'
+import { Pages } from './SubMenuProdutos'
 import './SubMenu.css'
 
-export default function SubMenu({ classR }) {
+export default function SubMenu({ classR, selectMenu }) {
+  const [subMenu, setSubMenu] = useState([])
+
+  useEffect(() => {
+    if(selectMenu) {
+      if(selectMenu === 'Pages') {
+        setSubMenu(Pages)
+      }
+    }
+  },[selectMenu])
   
   return (
       <ul className={"submenu " + classR}>
-        {Peges.map(({ name, link }) => (
+        {subMenu.map(({ name, link }) => (
           <li key={name} className={"submenu-li "  + classR}>
             <a className={ classR } href={link}>{name}</a>
           </li>
