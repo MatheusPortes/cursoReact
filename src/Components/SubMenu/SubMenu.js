027a69b1b88004822a2a056143687e42ed7dab31
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-06-13 18:01:20 -0300

    Correção de bug.

--- src/Components/SubMenu/SubMenu.js
+++ src/Components/SubMenu/SubMenu.js
@@ -3,27 +3,45 @@ import { Pages } from './SubMenuProdutos'
 import { Form } from './SubMenuForm'
 import './SubMenu.css'
 
-export default function SubMenu({ classR, selectMenu }) {
+export default function SubMenu({ selectMenu, name }) {
   const [subMenu, setSubMenu] = useState([])
+  const [showMenu, setShowMenu] = useState(false)
+
+  const submenuHandler = () => {
+    setShowMenu(!showMenu)
+  }
 
   useEffect(() => {
-    if(selectMenu) {
-      if(selectMenu === 'Pages') {
+    if (selectMenu) {
+      if (selectMenu === 'Pages') {
         setSubMenu(Pages)
       }
-      if(selectMenu === 'form') {
+      if (selectMenu === 'form') {
         setSubMenu(Form)
       }
     }
-  },[selectMenu])
-  
+  }, [selectMenu])
+
   return (
-      <ul className={"submenu " + classR}>
-        {subMenu.map(({ name, link }) => (
-          <li key={name} className={"submenu-li "  + classR}>
-            <a className={ classR } href={link}>{name}</a>
-          </li>
-        ))}
-      </ul>
+    <>
+      <li
+        className={'header-li'}
+        onMouseEnter={submenuHandler}
+        onMouseLeave={submenuHandler}
+      >
+        <a className="header-link" href="/FormReact">
+          {name}
+        </a>
+        {showMenu &&
+          <ul className={"submenu"}>
+            {subMenu.map(({ name, link }) => (
+              <li key={name} className={"submenu-li"}>
+                <a href={link}>{name}</a>
+              </li>
+            ))}
+          </ul>
+        }
+      </li>
+    </>
   )
 }
