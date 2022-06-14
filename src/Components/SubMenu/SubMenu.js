a74c5dbfc7d7570d8d1cfb82e866b2c59e370952
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-06-14 14:11:21 -0300

    M00-BUG01-V0.0.1: Correção do uso dos effect com async — Ajustes no menu. / Fixed using effects with async — Adjustments in the menu.

--- src/Components/SubMenu/SubMenu.js
+++ src/Components/SubMenu/SubMenu.js
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { Pages } from './SubMenuProdutos'
-import { Form } from './SubMenuForm'
+import React, { useState } from 'react'
 import './SubMenu.css'
 
 export default function SubMenu({ selectMenu, name }) {
-  const [subMenu, setSubMenu] = useState([])
   const [showMenu, setShowMenu] = useState(false)
 
   const submenuHandler = () => {
     setShowMenu(!showMenu)
   }
-
-  useEffect(() => {
-    if (selectMenu) {
-      if (selectMenu === 'Pages') {
-        setSubMenu(Pages)
-      }
-      if (selectMenu === 'form') {
-        setSubMenu(Form)
-      }
-    }
-  }, [selectMenu])
 
   return (
     <>
@@ -34,7 +20,7 @@ export default function SubMenu({ selectMenu, name }) {
         </a>
         {showMenu &&
           <ul className={"submenu"}>
-            {subMenu.map(({ name, link }) => (
+            {selectMenu.map(({ name, link }) => (
               <li key={name} className={"submenu-li"}>
                 <a href={link}>{name}</a>
               </li>
--- src/ReactContext/ContextGlobal.js
+++ src/ReactContext/ContextGlobal.js
@@ -7,9 +7,12 @@ export default function ContextGlobal({ children }) {
   const [loader, setLoader] = useState(false)
   const [listGlobalProdutos, setListGlobalProdutos] = useState([])
 
-  useEffect(async () => {
-    const data = await searchProduto()
-    setListGlobalProdutos(data.content)
+  useEffect(() => {
+    const run = async () => {
+      const data = await searchProduto()
+      setListGlobalProdutos(data.content)
+    }
+    run()
   },[])
 
   return (
