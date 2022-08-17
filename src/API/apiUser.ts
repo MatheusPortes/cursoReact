5645ebb4e0c8aacd3e731c2b0f6e6cc9a44afecf
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-08-16 23:11:31 -0300

    M04-A08-V0.0.1: API fetch dinamica.

--- src/API/apiUser.ts
+++ src/API/apiUser.ts
@@ -1,66 +1,180 @@
-export const Fetch = new Object({Get: Function, Post: Function, Put: Function, Delete: Function})
-
 export const API = async (URL: string): Promise<any> => {
-  const apiPromese = new Promise( (resolve, reject) => {
-    fetch(URL)
-      .then( (res:any) => {
-        if(!res.ok) throw new Error('Erro na requisição\nstatus: ', res.status, )
-        return res.json()
-      })
-      .then( dados => {
-        return resolve(dados)
-      })
-      .catch((error) => {
-        console.error(error)
-        return reject(error)
-      })
-  }) 
-  return apiPromese
-}
-
-// export const Get = async (URL: string): Promise<any> => {
-//   const apiPromese = new Promise( (resolve, reject) => {
-//     fetch(URL, {
-//       method: `GET`,
-
-//     })
-//       .then( (res:any) => {
-//         if(!res.ok) throw new Error('Erro na requisição\nstatus: ', res.status, )
-//         return res.json()
-//       })
-//       .then( dados => {
-//         return resolve(dados)
-//       })
-//       .catch((error) => {
-//         console.error(error)
-//         return reject(error)
-//       })
-//   }) 
-//   return apiPromese
-// }
-
-export const Post = async (URL: string, body: object): Promise<any> => {
-  const apiPromese = new Promise( (resolve, reject) => {
-    fetch(URL, {
-      method: `POST`,
-      headers: { 'Content-Type':`application/json` },
-      body: JSON.stringify(body),
+    const apiPromese = new Promise((resolve, reject) => {
+        API(URL)
+            .then((res: any) => {
+                if (!res.ok)
+                    throw new Error('Erro na requisição\nstatus: ', res.status)
+                return res.json()
+            })
+            .then((dados) => {
+                return resolve(dados)
+            })
+            .catch((error) => {
+                console.error(error)
+                return reject(error)
+            })
     })
-      .then( (res:any) => {
-        if(!res.ok) throw new Error('Erro na requisição\nstatus: ', res.status, )
-        return res.json()
-      })
-      .then( dados => {
-        return resolve(dados)
-      })
-      .catch((error) => {
-        console.error(error)
-        return reject(error)
-      })
-  }) 
-  return apiPromese
+    return apiPromese
 }
 
-// Fetch.Post = Post
+export const Get = async (
+    inputURL: string,
+    headers?: HeadersInit,
+    body?: BodyInit
+): Promise<any> => {
+    const apiPromese = new Promise((resolve, reject) => {
+        let fetchParams = {}
+
+        if (headers) {
+            fetchParams = { ...fetchParams, headers: headers }
+        }
+
+        if (body) {
+            fetchParams = { ...fetchParams, body: body }
+        }
+
+        fetch(inputURL, {
+            method: `GET`,
+            ...fetchParams,
+        })
+            .then((res: any) => {
+                if (!res.ok)
+                    throw new Error('Erro na requisição\nstatus: ', res.status)
+                return res.json()
+            })
+            .then((dados) => {
+                return resolve(dados)
+            })
+            .catch((error) => {
+                console.error(error)
+                return reject(error)
+            })
+    })
+    return apiPromese
+}
+
+export const Post = async (
+    URL: string,
+    body: object,
+    header: HeadersInit
+): Promise<any> => {
+    const fetchConfig: RequestInit = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { ...header },
+    }
+
+    const apiPromese = new Promise((resolve, reject) => {
+        fetch(URL, fetchConfig)
+            .then((res: any) => {
+                if (!res.ok)
+                    throw new Error('Erro na requisição\nstatus: ', res.status)
+                return res.json()
+            })
+            .then((dados) => {
+                return resolve(dados)
+            })
+            .catch((error) => {
+                console.error(error)
+                return reject(error)
+            })
+    })
+    return apiPromese
+}
+
+export const Put = async (
+    URL: string,
+    body: object,
+    header: HeadersInit
+): Promise<any> => {
+    const fetchConfig: RequestInit = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { ...header },
+    }
+
+    const apiPromese = new Promise((resolve, reject) => {
+        fetch(URL, fetchConfig)
+            .then((res: any) => {
+                if (!res.ok)
+                    throw new Error('Erro na requisição\nstatus: ', res.status)
+                return res.json()
+            })
+            .then((dados) => {
+                return resolve(dados)
+            })
+            .catch((error) => {
+                console.error(error)
+                return reject(error)
+            })
+    })
+    return apiPromese
+}
+
+export const Delete = async (
+    URL: string,
+    body: object,
+    header: HeadersInit
+): Promise<any> => {
+    const fetchConfig: RequestInit = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { ...header },
+    }
+
+    const apiPromese = new Promise((resolve, reject) => {
+        fetch(URL, fetchConfig)
+            .then((res: any) => {
+                if (!res.ok)
+                    throw new Error('Erro na requisição\nstatus: ', res.status)
+                return res.json()
+            })
+            .then((dados) => {
+                return resolve(dados)
+            })
+            .catch((error) => {
+                console.error(error)
+                return reject(error)
+            })
+    })
+    return apiPromese
+}
+
+export const Patch = async (
+    URL: string,
+    body: object,
+    header: HeadersInit
+): Promise<any> => {
+    const fetchConfig: RequestInit = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { ...header },
+    }
+
+    const apiPromese = new Promise((resolve, reject) => {
+        fetch(URL, fetchConfig)
+            .then((res: any) => {
+                if (!res.ok)
+                    throw new Error('Erro na requisição\nstatus: ', res.status)
+                return res.json()
+            })
+            .then((dados) => {
+                return resolve(dados)
+            })
+            .catch((error) => {
+                console.error(error)
+                return reject(error)
+            })
+    })
+    return apiPromese
+}
+
+export const Fetch = {
+    Get: Get,
+    Post: Post,
+    Put: Put,
+    Delete: Delete,
+    Patch: Patch,
+}
 
 export default API
